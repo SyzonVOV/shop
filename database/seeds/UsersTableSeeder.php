a2e028aa6bc6701fd93012d637c4713d4d9cdf6a
SyzonVOV <sizonenko89@gmail.com>
2020-04-26 22:03:54 +0300

    Edit on Windows10 Home

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Администратор',
+            'email' => 'admin@gov.ua',
+            'password' => bcrypt('admin'),
+            'is_admin' => 1,
+        ]);    }
+}
